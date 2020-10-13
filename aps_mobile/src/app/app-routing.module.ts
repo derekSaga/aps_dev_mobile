5150712c1660accbe2713ea48cd895a086d35835
Gustavo Jesus <gustavojspro@hotmail.com>
2020-10-13 03:39:16 -0300

    adicionando a pagina com os botões (sem funcionar nada ainda)

--- aps_mobile/src/app/app-routing.module.ts
+++ aps_mobile/src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'results',
+    loadChildren: () => import('./results/results.module').then( m => m.ResultsPageModule)
   },
+
 ];
 
 @NgModule({
