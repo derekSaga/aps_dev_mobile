2004499c1a4cdd92394efdc7e571a636dc90b660
Gustavo Jesus <gustavojspro@hotmail.com>
2020-10-13 05:35:22 -0300

    adicionando função de download pdf com erro para perguntar ao professor

--- aps_mobile/src/app/results/results.page.ts
+++ aps_mobile/src/app/results/results.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-results',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultsPage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    this.gerarPDF();
+   }
 
   ngOnInit() {
+  }
+
+  //https://www.youtube.com/watch?v=AdoPkp4KzFA
+  @ViewChild('content') content: ElementRef;
+  public gerarPDF(){
+    let doc = new jsPDF();
+
+    let specialElementHandlers = {
+      '#editor': function(element, renderer){
+        return true;
+      }
+    };
+
+    let content = this.content.nativeElement;
+
+    doc.fromHTML(content.innerHTML, 15, 15, {
+      'width': 190,
+      'elementHandlers': specialElementHandlers
+    });
+
+    doc.save('test.pdf');
+
   }
 
 }
