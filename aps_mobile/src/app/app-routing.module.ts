a6475718d2d6e83181b4d9f36ecffc21a2ae36c6
Jennifer <jennifersantossouza@hotmail.com>
2020-10-18 16:00:07 -0300

    Tela principal e Menu adicionados

--- aps_mobile/src/app/app-routing.module.ts
+++ aps_mobile/src/app/app-routing.module.ts
@@ -11,6 +11,17 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'menu',
+    loadChildren: () => import('./menu/menu.module').then( m => m.MenuPageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'menu',
+    pathMatch: 'full'
+  }, 
+  
+
 ];
 
 @NgModule({
