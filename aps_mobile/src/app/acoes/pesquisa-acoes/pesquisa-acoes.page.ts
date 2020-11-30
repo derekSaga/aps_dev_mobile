620e011487d6089824c85b09e5017db3fc42384e
ext.dereksgc <ext.derek.coelho@venergia.com.br>
2020-11-30 20:01:03 -0300

    fix

--- aps_mobile/src/app/acoes/pesquisa-acoes/pesquisa-acoes.page.ts
+++ aps_mobile/src/app/acoes/pesquisa-acoes/pesquisa-acoes.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {acoes} from '../todas/todas.page.js';
+import {getSymbol} from 'yahoo-stock-api';
 
 @Component({
   selector: 'app-pesquisa-acoes',
@@ -12,8 +13,13 @@ export class PesquisaAcoesPage implements OnInit {
   public nomeAcao = '';
   public resultadoPesquisa = null;
   public controll = false;
+  
+  // public teste(){
+  //   console.log(getSymbol('AAPL'));
+  // }
 
   public pesquisa_acao(){
+    // this.teste();
     this.resultadoPesquisa =  acoes.find(o => o.acao === this.nomeAcao.toUpperCase());
     if (this.resultadoPesquisa == null){
       this.controll = false;
