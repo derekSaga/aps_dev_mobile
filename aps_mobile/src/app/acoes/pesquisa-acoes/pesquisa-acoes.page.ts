a82102533cf818ad7e9406baafdb2815e709430a
ext.dereksgc <ext.derek.coelho@venergia.com.br>
2020-11-30 22:45:45 -0300

    lib stock-info e pesquisa-acoes alterado

--- aps_mobile/src/app/acoes/pesquisa-acoes/pesquisa-acoes.page.ts
+++ aps_mobile/src/app/acoes/pesquisa-acoes/pesquisa-acoes.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {acoes} from '../todas/todas.page.js';
-import {getSymbol} from 'yahoo-stock-api';
+import { acoes } from '../todas/todas.page.js';
+import { getSingleStockInfo } from 'stock-info';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-pesquisa-acoes',
@@ -12,24 +13,47 @@ export class PesquisaAcoesPage implements OnInit {
   // tslint:disable-next-line: variable-name
   public nomeAcao = '';
   public resultadoPesquisa = null;
+  public endereco = null;
   public controll = false;
-  
-  // public teste(){
-  //   console.log(getSymbol('AAPL'));
-  // }
 
-  public pesquisa_acao(){
-    // this.teste();
-    this.resultadoPesquisa =  acoes.find(o => o.acao === this.nomeAcao.toUpperCase());
-    if (this.resultadoPesquisa == null){
+  public async get_finance(quote_name) {
+    const url_finance = '//cors-anywhere.herokuapp.com/https://query1.finance.yahoo.com/v7/finance/quote?symbols=' + quote_name + '.SA'
+    try {
+      let resultado_finance = await this.Http.get(url_finance).toPromise();
+      resultado_finance = resultado_finance.quoteResponse.result[0];
+      return resultado_finance;
+    } catch (error) {
+      return null;
+    }
+  }
+
+  public async get_address(quote_name) {
+    try {
+      const address = '//cors-anywhere.herokuapp.com/https://query1.finance.yahoo.com/v10/finance/quoteSummary/' + this.nomeAcao + '.sa?modules=assetProfile%2CearningsHistory'
+      let resultado_address = await this.Http.get(address).toPromise();
+      resultado_address = resultado_address.quoteSummary.result[0].assetProfile;
+      
+      return resultado_address;
+    } catch (error) {
+      return null;
+    }
+  }
+
+  public async pesquisa_acao() {
+    let resultado_finance = await this.get_finance(this.nomeAcao);
+    let resultado_address = await this.get_address(this.nomeAcao);
+
+    if (resultado_finance == null) {
       this.controll = false;
       this.resultadoPesquisa = 'Ação não encontrada';
-    }else{
+    } else {
       this.controll = true;
+      this.resultadoPesquisa = resultado_finance;
+      this.endereco = resultado_address;
     }
     this.nomeAcao = '';
   }
-  constructor() { }
+  constructor(private Http: HttpClient) { }
 
   ngOnInit() {
   }
--- aps_mobile/src/app/app.module.ts
+++ aps_mobile/src/app/app.module.ts
@@ -10,12 +10,13 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { IonicStorageModule } from '@ionic/storage'; 
-
+import { HttpClientModule } from '@angular/common/http'
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule,
      IonicModule.forRoot(), 
+     HttpClientModule,
      AppRoutingModule,
      IonicStorageModule.forRoot()],     
   providers: [
