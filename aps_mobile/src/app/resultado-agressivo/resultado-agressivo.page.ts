4c34e309ab6757ab2b8954907faf0b2dda5f3131
Gustavo Jesus <gustavojspro@hotmail.com>
2020-12-05 00:37:15 -0300

    Finalizando as telas dos tipos de investimento

--- aps_mobile/src/app/resultado-agressivo/resultado-agressivo.page.ts
+++ aps_mobile/src/app/resultado-agressivo/resultado-agressivo.page.ts
@@ -33,35 +33,133 @@ export class ResultadoAgressivoPage implements OnInit {
       doc.text(textOffset, y, text);
     }
     centeredText(nome + ", seu perfil de investimento é " + tipo + "!", 10);
-    doc.text("Para pessoas com o seu perfil de investimentos recomendamos as Seguintes", 10, 30);
-    doc.text("aplicações para investir:", 10, 40);
+    //doc.text("Para pessoas com o seu perfil de investimentos recomendamos as Seguintes", 10, 30);
+    var lines;
+    var maxLength = doc.internal.pageSize.width-20;
+    if(prazo == "nsei"){
+      lines = doc.splitTextToSize('Pense no perfil agressivo não como alguém que “rasga dinheiro”. Pelo contrário: é alguém que tolera riscos, por ser o caminho que permitirá a ele potencializar seus resultados. Um investidor com perfil agressivo pode conviver com as oscilações naturais do mercado', maxLength);
+      
+      doc.text(lines,10, 30);
 
-    if(prazo == "asd" || prazo == "Agressivo"){
-      doc.setFontType("bold");
-      doc.text("Curto prazo:", 10, (prazo=="Agressivo"?60:90));
+      lines = doc.splitTextToSize('Em função dessas características, nada mais natural do que esse investidor ser quem melhor se identifica com a renda variável. Ele sabe que as oscilações fazem parte das características do mercado e dos ativos e, considerando o longo prazo, sabe também que elas são importantes para a valorização do seu investimento.', maxLength);
+      
+      doc.text(lines,10, 60);
+
+      lines = doc.splitTextToSize('Vamos recorrer a um exemplo para que você entenda melhor. Imagine um gráfico que ilustre um investimento em ações ao longo dos anos que vá de 0 a 100. Olhando de longe para ele, a impressão é que houve um movimento constante.', maxLength);
+      
+      doc.text(lines,10, 95);
+
+      lines = doc.splitTextToSize('Entretanto, quando você se aproxima, vê um movimento bastante peculiar, quase um zigue-zague, com os valores oscilando para cima e para baixo.', maxLength);
+      
+      doc.text(lines,10, 125); 
+
+      lines = doc.splitTextToSize('Mas, no final do ciclo, sempre oscilando mais para cima do que para baixo, a ponto de se configurar uma evolução.', maxLength);
+      
+      doc.text(lines,10, 145);
+
+      lines = doc.splitTextToSize('Agora, pense que cada oscilação para baixo, na realidade de uma empresa, representou um ciclo de desvalorização de seus ativos e, durante esse período, as ações chegaram a perder cerca de 20% de seu valor.', maxLength);
+      
+      doc.text(lines,10, 165);
+
+      lines = doc.splitTextToSize('Para o investidor iniciante, isso pode representar a senha para vender os ativos e se livrar daquele negócio o quanto antes, pois a tendência é perder dinheiro.', maxLength);
+      
+      doc.text(lines,10, 190); 
+
+      lines = doc.splitTextToSize('Não é assim para o investidor agressivo, porque ele acredita que o padrão será ascendente e que é uma questão de tempo até os resultados aparecerem.', maxLength);
+      
+      doc.text(lines,10, 215);
+      
+      lines = doc.splitTextToSize('O investidor agressivo geralmente tem mais conhecimento sobre o mercado que os demais.', maxLength);
+      
+      doc.text(lines,10, 240);
+
+      lines = doc.splitTextToSize('Esse perfil assume riscos sabendo onde está pisando e se prepara para eles. Não confunda isso com irresponsabilidade.', maxLength);
+      
+      doc.text(lines,10, 260);
+
+      lines = doc.splitTextToSize('Neste caso, agressividade tem muito mais a ver com estudo, e ousadia, do que com aposta.', maxLength);
+      
+      doc.text(lines,10, 280);
+
+      doc.addPage();
+
+      centeredText("Confira a lista de opções abaixo que o perfil agressivo mais investe:", 10);
+
       doc.setFontType("italic");
-      doc.text("Perfis agressivos tendem a não possuir investimentos a curto prazo.", 10, 70);
+      
+      doc.text("Ações",10, 30);
+      doc.text("Trader",10, 50);
+      doc.text("BDR",10, 70);
+      doc.text("Fundos Imobiliários",10, 90);
+      doc.text("Mercado futuro",10, 110);
+      doc.text("Debentures",10, 130);
+      doc.text("CRI/ CRA",10, 150);
+      doc.text("Fundos multimercado",10, 170);
     }
 
-    doc.setFontType("bold");
-    doc.text("Médio prazo:", 10, 90);
-    doc.setFontType("italic");
-    doc.text("Fundos Multimercado - 25% do seu capital para investimento", 10, 100);
-    doc.setFontType("italic");
-    doc.text("Debêntures - 25% do seu capital para investimento", 10, 110);
-    doc.setFontType("italic");
-    doc.text("CRI/CRA - 25% do seu capital para investimento", 10, 120);
-    doc.setFontType("italic");
-    doc.text("COE - 25% do seu capital para investimento", 10, 130);
+    if(prazo == "Longo"){
+      lines = doc.splitTextToSize('Perfil agressivo de investidor é o nome dado àqueles que buscam aumentar seu patrimônio, ainda que isto signifique correr mais riscos e enfrentar perdas de curto prazo ocasionais.', maxLength);
+      doc.text(lines,10, 30);
 
-    doc.setFontType("bold");
-    doc.text("Longo prazo:", 10, 150);
-    doc.setFontType("italic");
-    doc.text("Fundos Multimercado - 34% do seu capital para investimento", 10, 160);
-    doc.setFontType("italic");
-    doc.text("Debêntures - 33% do seu capital para investimento", 10, 170);
-    doc.setFontType("italic");
-    doc.text("CRI/CRA - 33% do seu capital para investimento", 10, 180);
+      lines = doc.splitTextToSize('Inclusive porque o risco e o rendimento de aplicações financeiras tendem a ser proporcionais. Quanto maior o risco, maior o potencial de retorno.', maxLength);
+      doc.text(lines,10, 55);
+
+      lines = doc.splitTextToSize('Logo, o perfil investidor agressivo costuma estar preparado para eventuais quedas de valores das suas ações e inclusive aproveitar alguns momentos de queda para comprar mais.', maxLength);
+      doc.text(lines,10, 75);
+
+      lines = doc.splitTextToSize('É comum que este investidor seja mais experiente, conhecendo melhor o funcionamento do mercado financeiro.', maxLength);
+      doc.text(lines,10, 100);
+
+      centeredText("Confira a lista de opções abaixo que o perfil agressivo mais investe:", 130);
+
+      doc.setFontType("italic");
+      
+      doc.text("Ações",10, 150);
+      doc.text("BDRs",10, 170);
+      doc.text("Mercado Futuro",10, 190);
+      doc.text("Operações Estruturadas com Opções",10, 210);
+    }
+
+    if(prazo == "Medio"){
+      lines = doc.splitTextToSize('O investidor com perfil agressivo tem um conhecimento maior do mercado financeiro. Ele entende a volatilidade do mercado e as oscilações sabendo que o benefício é de longo prazo.', maxLength);
+      doc.text(lines,10, 30);
+
+      lines = doc.splitTextToSize('A expectativa de retorno desse tipo de investidor é bastante alta, e ele não pode continuar diminuindo a economia.', maxLength);
+      doc.text(lines,10, 55);
+
+      lines = doc.splitTextToSize('Com esse cenário de juros baixos que estamos vivendo, os investidores buscam por investimentos de maior risco com expectativa de melhor retorno.', maxLength);
+      doc.text(lines,10, 75);
+
+      lines = doc.splitTextToSize('Mas cuidado: se você se desespera com a primeira queda, esse não é o seu perfil. Afinal, risco / retorno são diretamente proporcionais: quanto maior o rendimento, maior o risco atrelado a ele.', maxLength);
+      doc.text(lines,10, 100);
+
+      centeredText("Confira a lista de opções abaixo que o perfil agressivo mais investe:", 135);
+
+      doc.setFontType("italic");
+      
+      doc.text("Debentures",10, 155);
+      doc.text("COE",10, 175);
+      doc.text("CRI/ CRA",10, 195);
+      doc.text("Fundos multimercado",10, 215);
+    }
+
+    if(prazo == "Curto"){
+      lines = doc.splitTextToSize('O investidor com perfil agressivo tem um conhecimento maior do mercado financeiro. Ele entende a volatilidade do mercado e as oscilações sabendo que o benefício é de longo prazo.', maxLength);
+      doc.text(lines,10, 30);
+
+      lines = doc.splitTextToSize('A expectativa de retorno desse tipo de investidor é bastante alta, e ele não pode continuar diminuindo a economia.', maxLength);
+      doc.text(lines,10, 55);
+
+      lines = doc.splitTextToSize('Com esse cenário de juros baixos que estamos vivendo, os investidores buscam por investimentos de maior risco com expectativa de melhor retorno.', maxLength);
+      doc.text(lines,10, 75);
+
+      lines = doc.splitTextToSize('Mas cuidado: se você se desespera com a primeira queda, esse não é o seu perfil. Afinal, risco / retorno são diretamente proporcionais: quanto maior o rendimento, maior o risco atrelado a ele.', maxLength);
+      doc.text(lines,10, 100);
+
+      doc.setFontType("italic");
+      centeredText("Não é comum um perfil Agressivo investir a curto prazo!", 135);
+
+    }
     doc.save('investimentos.pdf');
   }
 
--- aps_mobile/src/app/resultado-moderado/resultado-moderado.page.ts
+++ aps_mobile/src/app/resultado-moderado/resultado-moderado.page.ts
@@ -26,7 +26,7 @@ export class ResultadoModeradoPage implements OnInit {
   /*https://www.youtube.com/watch?v=AdoPkp4KzFA -> antigo teste*/
 
   //https://mrrio.github.io/ -> tudo que podemos alterar
-  public async gerarPDF(nome, tipo) {
+  public async gerarPDF(nome, tipo, prazo) {
     const doc = new jsPDF();
     var centeredText = function (text, y) {
       var textWidth = doc.getStringUnitWidth(text) * doc.internal.getFontSize() / doc.internal.scaleFactor;
@@ -34,40 +34,112 @@ export class ResultadoModeradoPage implements OnInit {
       doc.text(textOffset, y, text);
     }
     centeredText(nome + ", seu perfil de investimento é " + tipo + "!", 10);
-    doc.text("Para pessoas com o seu perfil de investimentos recomendamos as Seguintes", 10, 30);
-    doc.text("aplicações para investir:", 10, 40);
+    //doc.text("Para pessoas com o seu perfil de investimentos recomendamos as Seguintes", 10, 30);
+    var lines;
+    var maxLength = doc.internal.pageSize.width-20;
+    if(prazo == "nsei"){
+      lines = doc.splitTextToSize('Perfil moderado é caracterizado por investidores que não abrem mão de segurança nas suas aplicações, mas que estão dispostos a correr alguns riscos para melhorar a sua rentabilidade.', maxLength);
+      doc.text(lines,10, 30);
 
-    doc.setFontType("bold");
-    doc.text("Curto prazo:", 10, 60);
-    doc.setFontType("italic");
-    doc.text("Fundo DI - 34% do seu capital para investimento", 10, 70);
-    doc.setFontType("italic");
-    doc.text("Fundo Crédito Privado - 33% do seu capital para investimento", 10, 80);
-    doc.setFontType("italic");
-    doc.text("Fundos Multimercado - 33% do seu capital para investimento", 10, 90);
+      lines = doc.splitTextToSize('Logo, ele fica entre o conservador e o agressivo, sendo moderado por não estar tão avesso a riscos, mas mantendo a cautela para não sofrer prejuízos desnecessários.', maxLength);
+      doc.text(lines,10, 60);
 
-    doc.setFontType("bold");
-    doc.text("Médio prazo:", 10, 110);
-    doc.setFontType("italic");
-    doc.text("CDB - 25% do seu capital para investimento", 10, 120);
-    doc.setFontType("italic");
-    doc.text("Fundos Multimercado - 25% do seu capital para investimento", 10, 130);
-    doc.setFontType("italic");
-    doc.text("Tesouro IPCA - 25% do seu capital para investimento", 10, 140);
-    doc.setFontType("italic");
-    doc.text("CRI/CRA - 25% do seu capital para investimento", 10, 150);
+      lines = doc.splitTextToSize('Este perfil de investidor ainda não tem interesse em ter uma grande parcela em renda variável mas também entende que a renda fixa pode não potenciar os ganhos no longo prazo.', maxLength);
+      doc.text(lines,10, 90);
+      
+      lines = doc.splitTextToSize('Sendo assim, separamos uma lista de possíveis investimentos para você.', maxLength);
+      doc.text(lines,10, 120); 
 
-    doc.setFontType("bold");
-    doc.text("Longo prazo:", 10, 170);
-    doc.setFontType("italic");
-    doc.text("Fundos Multimercado - 25% do seu capital para investimento", 10, 180);
-    doc.setFontType("italic");
-    doc.text("Tesouro IPCA - 25% do seu capital para investimento", 10, 190);
-    doc.setFontType("italic");
-    doc.text("CRI/CRA - 25% do seu capital para investimento", 10, 200);
-    doc.setFontType("italic");
-    doc.text("COE - 25% do seu capital para investimento", 10, 210);
-    doc.save('investimento.pdf');
+      centeredText("Confira a lista de opções abaixo que o perfil moderado mais investe:", 135);
+
+      doc.setFontType("italic");
+      
+      doc.text("Tesouro IPCA",10, 155);
+      doc.text("CRI/ CRA",10, 175);
+      doc.text("Fundos multimercado",10, 195);
+      doc.text("Crédito Privado",10, 215);
+      doc.text("DI",10, 235);
+      doc.text("Multimercado",10, 255);
+      doc.text("COE",10, 275);
+    }
+
+    if(prazo == "Longo"){
+      lines = doc.splitTextToSize('Perfil moderado é caracterizado por investidores que não abrem mão de segurança nas suas aplicações, mas que estão dispostos a correr alguns riscos para melhorar a sua rentabilidade.', maxLength);
+      doc.text(lines,10, 30);
+
+      lines = doc.splitTextToSize('O que você precisa saber para investir a Longo Prazo:', maxLength);
+      doc.text(lines,10, 55);
+
+      lines = doc.splitTextToSize('Existem muitas vantagens no planejamento de investimentos a longo prazo. O maior deles é que a organização para um período maior permite uma melhor diversificação de sua carteira e, com isso, maiores ganhos e menos riscos.', maxLength);
+      doc.text(lines,10, 70);
+
+      lines = doc.splitTextToSize('Pontualmente, diversos investimentos com prazo maior tendem a cobrar menos Imposto de Renda e oferecer taxas de retorno superiores.', maxLength);
+      doc.text(lines,10, 100);
+
+      centeredText("Confira a lista de opções abaixo:", 120);
+
+      doc.setFontType("italic");
+      
+      doc.text("Tesouro IPCA",10, 140);
+      doc.text("Fundos multimercado",10, 160);
+      doc.text("CRI/ CRA",10, 180);
+      doc.text("COE",10, 200);
+    }
+
+    if(prazo == "Medio"){
+      lines = doc.splitTextToSize('Perfil de Investidor Moderado busca a preservação de recursos, com tolerância zero a perda e alta liquidez', maxLength);
+      doc.text(lines,10, 30);
+
+      lines = doc.splitTextToSize('O que você precisa saber para investir a Médio Prazo:', maxLength);
+      doc.text(lines,10, 50);
+
+      lines = doc.splitTextToSize('Investimentos de médio prazo são aqueles cujo retorno será obtido, em média, entre dois e cinco anos – assim como o seu resgate. Em geral, os aportes são feitos pelo investidor que visa determinados objetivos no médio prazo, como viagens, a compra de um carro ou outro bem de alto valor, entre outros.', maxLength);
+      doc.text(lines,10, 65);
+
+      lines = doc.splitTextToSize('Para que estes objetivos possam ser alcançados por meio dos investimentos de médio prazo, e fundamental que o investidor realize os aportes de maneira correta, analisando a rentabilidade e a liquidez do investimento. Desta forma, ele evitará comprometer sua organização financeira e conseguirá atingir seus objetivos de maneira mais fácil e planejada.', maxLength);
+      doc.text(lines,10, 105);
+
+      lines = doc.splitTextToSize('Investir em determinados produtos de renda fixa, fundos multimercados, entre outros, por exemplo, podem ser boas opções de composição da sua carteira de investimentos de médio prazo.', maxLength);
+      doc.text(lines,10, 155);
+
+      centeredText("Confira a lista de opções abaixo:", 185);
+
+      doc.setFontType("italic");
+      
+      doc.text("Tesouro IPCA",10, 205);
+      doc.text("Fundos multimercado",10, 225);
+      doc.text("CRI/ CRA",10, 245);
+      doc.text("COE",10, 265);
+    }
+
+    if(prazo == "Curto"){
+      lines = doc.splitTextToSize('Para pessoas com perfil Moderado, que já sabem o objetivo final dos seus investimentos e avaliaram que precisam de resultados em até 2 anos, temos algumas sujestões para investir da melhor forma, vendo o que realmente faz sentido para você.', maxLength);
+      doc.text(lines,10, 30);
+
+      lines = doc.splitTextToSize('O que você precisa saber para investir a Curto Prazo', maxLength);
+      doc.text(lines,10, 65);
+
+      lines = doc.splitTextToSize('Como aqui entram os objetivos a serem atingidos em um espaço curto de tempo, são duas as principais características de que devem ser analisadas: o risco e a liquidez.', maxLength);
+      doc.text(lines,10, 80);
+
+      lines = doc.splitTextToSize('É importante que seja possível resgatar o dinheiro e seus rendimentos no momento que desejar ou precisar, sem dificuldades – por isso, quanto maior a liquidez, melhor.', maxLength);
+      doc.text(lines,10, 110);
+
+      lines = doc.splitTextToSize('Em relação ao risco, ele é basicamente a incerteza que se pode ter em relação ao rendimento: quanto maior o risco, maiores as chances de ter uma rentabilidade maior e de ganhar muito dinheiro, mas também de se perder e ter uma rentabilidade negativa, por exemplo.', maxLength);
+      doc.text(lines,10, 140);
+
+      lines = doc.splitTextToSize('No caso de estratégias de curto prazo, o recomendado é que o risco seja baixo para que sejam evitadas possíveis perdas – e se garanta uma rentabilidade mínima e segura. Afinal, com um período curto de tempo, pode ser difícil recuperar rentabilidade perdida.', maxLength);
+      doc.text(lines,10, 175);
+
+      centeredText("Confira a lista de opções abaixo:", 210);
+
+      doc.setFontType("italic");
+      
+      doc.text("DI",10, 230);
+      doc.text("Crédito Privado",10, 250);
+      doc.text("Fundos multimercado",10, 270);
+    }
+    doc.save('investimentos.pdf');
   }
 
 }
