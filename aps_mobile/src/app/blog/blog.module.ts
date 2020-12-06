5c228eeef00a7ba7c0ee9eb1bb1caea07f4a9b1b
ext.dereksgc <ext.derek.coelho@venergia.com.br>
2020-12-06 20:44:00 -0300

    versão final fix

--- aps_mobile/src/app/blog/blog.module.ts
+++ aps_mobile/src/app/blog/blog.module.ts
@@ -1,3 +1,5 @@
+import { Modal2Component } from './../modal2/modal2/modal2.component';
+import { Modal3Component } from './../modal2/modal3/modal3.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -17,7 +19,7 @@ import { ModalComponent2 } from '../modal2/modal/modal.component';
     IonicModule,
     BlogPageRoutingModule
   ],
-  declarations: [BlogPage, ModalComponent, ModalComponent2],
-  entryComponents: [ModalComponent,  ModalComponent2]
+  declarations: [BlogPage, ModalComponent, ModalComponent2, Modal3Component, Modal2Component],
+  entryComponents: [ModalComponent,  ModalComponent2, Modal3Component, Modal2Component]
 })
 export class BlogPageModule {}
